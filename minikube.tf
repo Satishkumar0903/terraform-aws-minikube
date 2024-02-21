554ce629e2728add247194e24138184ddfb39738
unknown <bsatheesh779@gmail.com>
2024-02-21 21:25:57 +0530

    roboshop_minikube

---
+++ minikube.tf
@@ -0,0 +1,23 @@
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region    = "us-east-1"
+  cluster_name  = "roboshop"
+  aws_instance_type = "t3.medium"
+  ssh_public_key = "~/Users/lenovo/awspractice.pub"
+  aws_subnet_id = "subnet-06108d218328020e2"
+  ami_image_id = "ami-b81dbfc5"#by default centos 7 will be used
+  hosted_zone = "devops-practice.site"
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
